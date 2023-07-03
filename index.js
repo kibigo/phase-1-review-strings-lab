19d599c023aed3bbf29211a76586785aba3d14d1
Kibigo <jacksonkibigo99@gmail.com>
2023-07-03 20:57:29 +0300

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const currentUser = "Grace Hopper";
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser[0]}!`;
-
-
-
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!` ;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser[0]}!`
